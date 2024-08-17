f8a2db2043ec61f32a88fa42aaa61d8265ffa58c
Flobul <46993341+Flobul@users.noreply.github.com>
2024-08-17 14:48:28 +0000

    Auto check cmdname and enums of php file

---
+++ cmdName.php
@@ -0,0 +1,20 @@
+<?php return array (
+  "alarmSystem" => 
+  array (
+    "sendEvent(alarmSystemStatus,off)" => __("sendOffEvent", __FILE__),
+    "sendEvent(alarmSystemStatus,away)" => __("sendAwayEvent", __FILE__),
+    "sendEvent(alarmSystemStatus,stay)" => __("sendStayEvent", __FILE__),
+  ),
+  "alarmSystemArmOnly" => 
+  array (
+    "sendEvent(alarmSystemStatus,away)" => __("sendAwayEvent", __FILE__),
+    "sendEvent(alarmSystemStatus,stay)" => __("sendStayEvent", __FILE__),
+  ),
+  "astronomicalData" => 
+  array (
+    "sendEvent(sunriseTime)" => __("sendEvent(sunriseTime)", __FILE__),
+    "sendEvent(sunrise)" => __("sendEvent(sunrise)", __FILE__),
+    "sendEvent(sunset)" => __("sendEvent(sunset)", __FILE__),
+    "sendEvent(sunsetTime)" => __("sendEvent(sunsetTime)", __FILE__),
+  ),
+);
