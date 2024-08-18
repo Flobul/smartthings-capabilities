f671d90d0d517dd89028e0e57abda317ef16b139
Flobul <46993341+Flobul@users.noreply.github.com>
2024-08-18 10:19:57 +0000

    Auto check cmdname and enums of php file

--- enums.php
+++ enums.php
@@ -1,2 +1,14 @@
 <?php return array (
+  "supportedAcModes" => 
+  array (
+    "auto" => __("auto", __FILE__),
+    "cool" => __("cool", __FILE__),
+    "dry" => __("dry", __FILE__),
+    "coolClean" => __("coolClean", __FILE__),
+    "dryClean" => __("dryClean", __FILE__),
+    "fanOnly" => __("fanOnly", __FILE__),
+    "heat" => __("heat", __FILE__),
+    "heatClean" => __("heatClean", __FILE__),
+    "notSupported" => __("notSupported", __FILE__),
+  ),
 );
