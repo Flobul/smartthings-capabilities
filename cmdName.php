17b6f314edad78da69b2dbac9b52e89b5817a0d3
Flobul <46993341+Flobul@users.noreply.github.com>
2024-08-27 10:46:06 +0000

    Auto check cmdname and enums of php file

--- cmdName.php
+++ cmdName.php
@@ -17,4 +17,16 @@
     "sendEvent(sunset)" => __("sendEvent(sunset)", __FILE__),
     "sendEvent(sunsetTime)" => __("sendEvent(sunsetTime)", __FILE__),
   ),
+  "samsungTV" => 
+  array (
+    "setPictureMode" => __("setPictureMode", __FILE__),
+    "volumeDown" => __("volumeDown", __FILE__),
+    "setSoundMode" => __("setSoundMode", __FILE__),
+    "showMessage" => __("showMessage", __FILE__),
+    "volumeUp" => __("volumeUp", __FILE__),
+    "unmute" => __("unmute", __FILE__),
+    "setVolume" => __("setVolume", __FILE__),
+    "off" => __("off", __FILE__),
+    "on" => __("on", __FILE__),
+  ),
 );
