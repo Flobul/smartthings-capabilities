35266a1ff532b130089d5a118018d790fcca7c3c
Flobul <46993341+Flobul@users.noreply.github.com>
2024-11-19 17:49:47 +0000

    Auto check cmdname and enums of php file

--- enums.php
+++ enums.php
@@ -1,2 +1,14 @@
 <?php return array (
+  "operatingState" => 
+  array (
+    "autoClean" => __("autoClean", __FILE__),
+    "speedClean" => __("speedClean", __FILE__),
+    "quietClean" => __("quietClean", __FILE__),
+    "timedClean" => __("timedClean", __FILE__),
+  ),
+  "deodorFilterStatus" => 
+  array (
+    "preannouncement" => __("preannouncement", __FILE__),
+    "uvregeneration" => __("uvregeneration", __FILE__),
+  ),
 );
