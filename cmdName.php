01d5c16bfeabfb14535f24d43205486cc69c9170
Flobul <46993341+Flobul@users.noreply.github.com>
2024-11-23 08:44:08 +0000

    Auto check cmdname and enums of php file

--- cmdName.php
+++ cmdName.php
@@ -1,14 +1,14 @@
 <?php return array (
   "alarmSystem" => 
   array (
-    "sendEvent(alarmSystemStatus,off)" => __("sendOffEvent", __FILE__),
-    "sendEvent(alarmSystemStatus,away)" => __("sendAwayEvent", __FILE__),
-    "sendEvent(alarmSystemStatus,stay)" => __("sendStayEvent", __FILE__),
+    "sendEvent(alarmSystemStatus,off)" => __("sendEvent(alarmSystemStatus,off)", __FILE__),
+    "sendEvent(alarmSystemStatus,away)" => __("sendEvent(alarmSystemStatus,away)", __FILE__),
+    "sendEvent(alarmSystemStatus,stay)" => __("sendEvent(alarmSystemStatus,stay)", __FILE__),
   ),
   "alarmSystemArmOnly" => 
   array (
-    "sendEvent(alarmSystemStatus,away)" => __("sendAwayEvent", __FILE__),
-    "sendEvent(alarmSystemStatus,stay)" => __("sendStayEvent", __FILE__),
+    "sendEvent(alarmSystemStatus,away)" => __("sendEvent(alarmSystemStatus,away)", __FILE__),
+    "sendEvent(alarmSystemStatus,stay)" => __("sendEvent(alarmSystemStatus,stay)", __FILE__),
   ),
   "astronomicalData" => 
   array (
@@ -16,17 +16,5 @@
     "sendEvent(sunrise)" => __("sendEvent(sunrise)", __FILE__),
     "sendEvent(sunset)" => __("sendEvent(sunset)", __FILE__),
     "sendEvent(sunsetTime)" => __("sendEvent(sunsetTime)", __FILE__),
-  ),
-  "samsungTV" => 
-  array (
-    "setPictureMode" => __("setPictureMode", __FILE__),
-    "volumeDown" => __("volumeDown", __FILE__),
-    "setSoundMode" => __("setSoundMode", __FILE__),
-    "showMessage" => __("showMessage", __FILE__),
-    "volumeUp" => __("volumeUp", __FILE__),
-    "unmute" => __("unmute", __FILE__),
-    "setVolume" => __("setVolume", __FILE__),
-    "off" => __("off", __FILE__),
-    "on" => __("on", __FILE__),
   ),
 );
