3bd8046d5f0dcaa12871a755f1e199e47514bb31
Flobul <46993341+Flobul@users.noreply.github.com>
2024-12-15 09:55:20 +0000

    Auto check cmdname and enums of php file

--- cmdName.php
+++ cmdName.php
@@ -17,4 +17,21 @@
     "sendEvent(sunset)" => __("sendEvent(sunset)", __FILE__),
     "sendEvent(sunsetTime)" => __("sendEvent(sunsetTime)", __FILE__),
   ),
+  "dishwasherMode" => 
+  array (
+    "supportedDishwasherModes" => __("supported dishwasher modes", __FILE__),
+  ),
+  "dishwasherOperatingState" => 
+  array (
+    "progress" => __("progress", __FILE__),
+  ),
+  "ovenSetpoint" => 
+  array (
+    "ovenSetpointRange" => __("Oven setpoint range", __FILE__),
+  ),
+  "securitySystem" => 
+  array (
+    "supportedSecuritySystemCommands" => __("supported security system commands", __FILE__),
+    "supportedSecuritySystemStatuses" => __("supported security system statuses", __FILE__),
+  ),
 );
