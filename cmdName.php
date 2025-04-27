ebe35cc5a36e175ca58869362df78cea937a6562
Flobul <46993341+Flobul@users.noreply.github.com>
2025-04-27 10:42:42 +0000

    Auto check cmdname and enums of php file

--- cmdName.php
+++ cmdName.php
@@ -17,4 +17,34 @@
     "sendEvent(sunset)" => __("sendEvent(sunset)", __FILE__),
     "sendEvent(sunsetTime)" => __("sendEvent(sunsetTime)", __FILE__),
   ),
+  "custom.supportedOptions" => 
+  array (
+    "course" => __("course", __FILE__),
+    "setCourse" => __("setCourse", __FILE__),
+  ),
+  "dishwasherMode" => 
+  array (
+    "supportedDishwasherModes" => __("supported dishwasher modes", __FILE__),
+  ),
+  "dishwasherOperatingState" => 
+  array (
+    "progress" => __("progress", __FILE__),
+  ),
+  "ovenSetpoint" => 
+  array (
+    "ovenSetpointRange" => __("Oven setpoint range", __FILE__),
+  ),
+  "samsungvd.ambient" => 
+  array (
+    "info" => __("info", __FILE__),
+  ),
+  "samsungvd.audioInputSource" => 
+  array (
+    "setNextInputSource" => __("setNextInputSource", __FILE__),
+  ),
+  "securitySystem" => 
+  array (
+    "supportedSecuritySystemCommands" => __("supported security system commands", __FILE__),
+    "supportedSecuritySystemStatuses" => __("supported security system statuses", __FILE__),
+  ),
 );
--- enums.php
+++ enums.php
@@ -1,2 +1,15 @@
 <?php return array (
+  "chargingState" => 
+  array (
+    "other" => __("other", __FILE__),
+  ),
+  "ovenMode" => 
+  array (
+    "StrongSteam" => __("Strong Steam", __FILE__),
+    "Rinse" => __("Rinse", __FILE__),
+  ),
+  "dryerJobState" => 
+  array (
+    "laundrySensing" => __("laundrySensing", __FILE__),
+  ),
 );
