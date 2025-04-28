6e815f2cd14f0abc33dcb28f9d76aeb78335437f
Flobul <46993341+Flobul@users.noreply.github.com>
2025-04-28 16:48:54 +0000

    Auto check cmdname and enums of php file

--- cmdName.php
+++ cmdName.php
@@ -17,4 +17,131 @@
     "sendEvent(sunset)" => __("sendEvent(sunset)", __FILE__),
     "sendEvent(sunsetTime)" => __("sendEvent(sunsetTime)", __FILE__),
   ),
+  "custom.supportedOptions" => 
+  array (
+    "course" => __("course", __FILE__),
+    "setCourse" => __("setCourse", __FILE__),
+  ),
+  "deliveryRobotCall" => 
+  array (
+    "robotState" => __("Robot de distribution", __FILE__),
+    "supportedRobotStates" => __("Supported delivery robot states", __FILE__),
+    "supportedRobotCommands" => __("Supported delivery robot commands", __FILE__),
+    "call" => __("Appeler", __FILE__),
+    "cancel" => __("Annulé", __FILE__),
+  ),
+  "dishwasherMode" => 
+  array (
+    "supportedDishwasherModes" => __("supported dishwasher modes", __FILE__),
+  ),
+  "dishwasherOperatingState" => 
+  array (
+    "progress" => __("progress", __FILE__),
+  ),
+  "endToEndEncryption" => 
+  array (
+    "nonce" => __("nonce", __FILE__),
+    "encryptedKek" => __("encryptedKek", __FILE__),
+    "generateNonce" => __("generateNonce", __FILE__),
+    "encryptKek" => __("encryptKek", __FILE__),
+  ),
+  "flowMeasurement" => 
+  array (
+    "flowRange" => __("flow range", __FILE__),
+    "flow" => __("Mesure du flux", __FILE__),
+  ),
+  "massageIntensityChange" => 
+  array (
+    "supportedPositions" => __("Supported Positions", __FILE__),
+    "nextIntensity" => __("Massage", __FILE__),
+  ),
+  "massageIntensityControl" => 
+  array (
+    "intensityFoot" => __("Intensité aux pieds", __FILE__),
+    "supportedPositions" => __("Supported Positions", __FILE__),
+    "intensityWhole" => __("Intensité", __FILE__),
+    "intensityHead" => __("Intensité à la tête", __FILE__),
+    "intensityRange" => __("Intensity of Range", __FILE__),
+    "setIntensityFoot" => __("setIntensityFoot", __FILE__),
+    "setIntensityWhole" => __("setIntensityWhole", __FILE__),
+    "setIntensityHead" => __("setIntensityHead", __FILE__),
+  ),
+  "massageOperating" => 
+  array (
+    "massageState" => __("Massage", __FILE__),
+    "stop" => __("Désactivé", __FILE__),
+    "start" => __("Activé", __FILE__),
+  ),
+  "massageOperatingState" => 
+  array (
+    "massageState" => __("Massage", __FILE__),
+  ),
+  "massageTimeChange" => 
+  array (
+    "nextTime" => __("Temps", __FILE__),
+  ),
+  "massageTimeControl" => 
+  array (
+    "completionTime" => __("Temps", __FILE__),
+    "supportedSetTimes" => __("Supported Set Times", __FILE__),
+    "setTime" => __("Temps", __FILE__),
+  ),
+  "motionBed" => 
+  array (
+    "mode" => __("Mouvement", __FILE__),
+    "supportedMotionPositions" => __("Supported Motion Positions", __FILE__),
+    "statelessMode" => __("Stateless Mode", __FILE__),
+    "supportedModes" => __("Supported Modes", __FILE__),
+    "stop" => __("stop", __FILE__),
+    "setMode" => __("Set mode", __FILE__),
+    "start" => __("start", __FILE__),
+  ),
+  "ovenSetpoint" => 
+  array (
+    "ovenSetpointRange" => __("Oven setpoint range", __FILE__),
+  ),
+  "petActivity" => 
+  array (
+    "supportedPetActivities" => __("Supported pet activities", __FILE__),
+    "petActivity" => __("Activité de l\"animal", __FILE__),
+  ),
+  "safetySwitch" => 
+  array (
+    "switch" => __("switch", __FILE__),
+    "off" => __("off", __FILE__),
+  ),
+  "safetyValve" => 
+  array (
+    "valve" => __("valve", __FILE__),
+    "close" => __("close", __FILE__),
+  ),
+  "samsungce.washerCycle" => 
+  array (
+    "cycleType" => __("cycleType", __FILE__),
+  ),
+  "samsungvd.ambient" => 
+  array (
+    "info" => __("info", __FILE__),
+  ),
+  "samsungvd.audioInputSource" => 
+  array (
+    "setNextInputSource" => __("setNextInputSource", __FILE__),
+  ),
+  "securitySystem" => 
+  array (
+    "supportedSecuritySystemCommands" => __("supported security system commands", __FILE__),
+    "supportedSecuritySystemStatuses" => __("supported security system statuses", __FILE__),
+  ),
+  "serviceArea" => 
+  array (
+    "selectedAreas" => __("Zone de nettoyage", __FILE__),
+    "supportedAreas" => __("supported areas", __FILE__),
+    "selectAreas" => __("select areas", __FILE__),
+  ),
+  "wifiInformation" => 
+  array (
+    "supportedWiFiAuthTypes" => __("supportedWiFiAuthTypes", __FILE__),
+    "ssid" => __("ssid", __FILE__),
+    "supportedWiFiFrequencies" => __("supportedWiFiFrequencies", __FILE__),
+  ),
 );
--- enums.php
+++ enums.php
@@ -1,2 +1,59 @@
 <?php return array (
+  "chargingState" => 
+  array (
+    "other" => __("Other", __FILE__),
+  ),
+  "robotState" => 
+  array (
+    "available" => __("Mise en attente pour la livraison", __FILE__),
+    "using" => __("En cours d\"utilisation", __FILE__),
+    "error" => __("Erreur", __FILE__),
+    "called" => __("Appelé", __FILE__),
+    "callFailed" => __("Impossible de contacter le robot", __FILE__),
+  ),
+  "mode" => 
+  array (
+    "preset1" => __("Ma trousse", __FILE__),
+    "preset2" => __("Preset 2", __FILE__),
+    "zeroG" => __("Gravité zéro", __FILE__),
+    "flat" => __("Plat", __FILE__),
+    "tv" => __("TV", __FILE__),
+    "reading" => __("Lecture", __FILE__),
+    "antiSnore" => __("Anti-ronflement", __FILE__),
+    "noMode" => __("noMode", __FILE__),
+  ),
+  "ovenMode" => 
+  array (
+    "StrongSteam" => __("Strong Steam", __FILE__),
+    "Rinse" => __("Rinse", __FILE__),
+  ),
+  "petActivity" => 
+  array (
+    "idle" => __("En veille", __FILE__),
+    "eating" => __("Alimentation", __FILE__),
+    "pooping" => __("Toilettes (déchets solides)", __FILE__),
+    "sleeping" => __("En veille", __FILE__),
+    "drinkingWater" => __("Hydratation", __FILE__),
+    "peeing" => __("Toilettes (urine)", __FILE__),
+    "playing" => __("Jeu", __FILE__),
+    "resting" => __("Repos", __FILE__),
+    "walking" => __("Marche", __FILE__),
+    "running" => __("Course", __FILE__),
+    "barking" => __("Aboiement", __FILE__),
+    "meowing" => __("Miaulement", __FILE__),
+  ),
+  "dryerJobState" => 
+  array (
+    "laundrySensing" => __("Détection du linge", __FILE__),
+  ),
+  "cycleType" => 
+  array (
+    "allInOne" => __("allInOne", __FILE__),
+    "washingOnly" => __("washingOnly", __FILE__),
+    "dryingOnly" => __("dryingOnly", __FILE__),
+  ),
+  "protocolType" => 
+  array (
+    "l3_ocf" => __("l3_ocf", __FILE__),
+  ),
 );
