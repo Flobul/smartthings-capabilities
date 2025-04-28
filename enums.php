172863b8105ffb44320150c680386c716ffbfa63
Flobul <46993341+Flobul@users.noreply.github.com>
2025-04-28 06:49:10 +0000

    Auto check cmdname and enums of php file

--- enums.php
+++ enums.php
@@ -1,2 +1,25 @@
 <?php return array (
+  "chargingState" => 
+  array (
+    "other" => __("Other", __FILE__),
+  ),
+  "ovenMode" => 
+  array (
+    "StrongSteam" => __("Strong Steam", __FILE__),
+    "Rinse" => __("Rinse", __FILE__),
+  ),
+  "dryerJobState" => 
+  array (
+    "laundrySensing" => __("Détection du linge", __FILE__),
+  ),
+  "cycleType" => 
+  array (
+    "allInOne" => __("allInOne", __FILE__),
+    "washingOnly" => __("washingOnly", __FILE__),
+    "dryingOnly" => __("dryingOnly", __FILE__),
+  ),
+  "protocolType" => 
+  array (
+    "l3_ocf" => __("l3_ocf", __FILE__),
+  ),
 );
