9db3be5a01c6960e364a3dd4d1a10d9773c69c0b
Flobul <46993341+Flobul@users.noreply.github.com>
2025-04-28 16:46:40 +0000

    Auto check cmdname and enums of php file

--- enums.php
+++ enums.php
@@ -1,2 +1,40 @@
 <?php return array (
+  "chargingState" => 
+  array (
+    "other" => __("Other", __FILE__),
+  ),
+  "ovenMode" => 
+  array (
+    "StrongSteam" => __("Strong Steam", __FILE__),
+    "Rinse" => __("Rinse", __FILE__),
+  ),
+  "petActivity" => 
+  array (
+    "idle" => __("En veille", __FILE__),
+    "eating" => __("Alimentation", __FILE__),
+    "pooping" => __("Toilettes (déchets solides)", __FILE__),
+    "sleeping" => __("En veille", __FILE__),
+    "drinkingWater" => __("Hydratation", __FILE__),
+    "peeing" => __("Toilettes (urine)", __FILE__),
+    "playing" => __("Jeu", __FILE__),
+    "resting" => __("Repos", __FILE__),
+    "walking" => __("Marche", __FILE__),
+    "running" => __("Course", __FILE__),
+    "barking" => __("Aboiement", __FILE__),
+    "meowing" => __("Miaulement", __FILE__),
+  ),
+  "dryerJobState" => 
+  array (
+    "laundrySensing" => __("Détection du linge", __FILE__),
+  ),
+  "cycleType" => 
+  array (
+    "allInOne" => __("allInOne", __FILE__),
+    "washingOnly" => __("washingOnly", __FILE__),
+    "dryingOnly" => __("dryingOnly", __FILE__),
+  ),
+  "protocolType" => 
+  array (
+    "l3_ocf" => __("l3_ocf", __FILE__),
+  ),
 );
