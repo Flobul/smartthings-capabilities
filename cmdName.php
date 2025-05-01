5097327e8b1821a8fa5bca06c59e996e3a8df0ae
Flobul <46993341+Flobul@users.noreply.github.com>
2025-05-01 09:03:15 +0000

    Auto check cmdname and enums of php file

--- cmdName.php
+++ cmdName.php
@@ -1,4 +1,38 @@
 <?php return array (
+  "abateachieve62503.statelessFanspeedDecrease" => 
+  array (
+    "fanspeedDown" => __("fanspeedDown", __FILE__),
+  ),
+  "abateachieve62503.statelessFanspeedIncrease" => 
+  array (
+    "fanspeedUp" => __("fanspeedUp", __FILE__),
+  ),
+  "abateachieve62503.statelessPowerOff" => 
+  array (
+    "powerOff" => __("powerOff", __FILE__),
+  ),
+  "abateachieve62503.statelessPowerOn" => 
+  array (
+    "powerOn" => __("powerOn", __FILE__),
+  ),
+  "abateachieve62503.statelessTemperatureDown" => 
+  array (
+    "temperatureDown" => __("temperatureDown", __FILE__),
+  ),
+  "abateachieve62503.statelessTemperatureUp" => 
+  array (
+    "temperatureUp" => __("temperatureUp", __FILE__),
+  ),
+  "airQualityHealthConcern" => 
+  array (
+    "supportedAirQualityValues" => __("Supported air quality values", __FILE__),
+  ),
+  "alarmSensor" => 
+  array (
+    "supportedAlarmSensorStates" => __("supportedAlarmSensorStates", __FILE__),
+    "alarmSensorState" => __("alarmSensorState", __FILE__),
+    "setAlarmSensorState" => __("setAlarmSensorState", __FILE__),
+  ),
   "alarmSystem" => 
   array (
     "sendEvent(alarmSystemStatus,off)" => __("sendEvent(alarmSystemStatus,off)", __FILE__),
@@ -10,11 +44,189 @@
     "sendEvent(alarmSystemStatus,away)" => __("sendEvent(alarmSystemStatus,away)", __FILE__),
     "sendEvent(alarmSystemStatus,stay)" => __("sendEvent(alarmSystemStatus,stay)", __FILE__),
   ),
+  "antiSnoringPillow" => 
+  array (
+    "snoringTime" => __("snoringTime", __FILE__),
+    "supportPillowOperationTimeGraph" => __("supportPillowOperationTimeGraph", __FILE__),
+    "snoringTimeDelta" => __("snoringTimeDelta", __FILE__),
+    "pillowOperationTime" => __("pillowOperationTime", __FILE__),
+    "state" => __("state", __FILE__),
+    "supportSnoringTimeGraph" => __("supportSnoringTimeGraph", __FILE__),
+    "pillowOperationTimeDelta" => __("pillowOperationTimeDelta", __FILE__),
+    "off" => __("off", __FILE__),
+    "on" => __("on", __FILE__),
+  ),
   "astronomicalData" => 
   array (
     "sendEvent(sunriseTime)" => __("sendEvent(sunriseTime)", __FILE__),
     "sendEvent(sunrise)" => __("sendEvent(sunrise)", __FILE__),
     "sendEvent(sunset)" => __("sendEvent(sunset)", __FILE__),
     "sendEvent(sunsetTime)" => __("sendEvent(sunsetTime)", __FILE__),
+  ),
+  "batteryLevel" => 
+  array (
+    "quantity" => __("quantity", __FILE__),
+    "type" => __("type", __FILE__),
+  ),
+  "coffeeMakerOperation" => 
+  array (
+    "errorState" => __("errorState", __FILE__),
+    "coffeeMakerOperationStatus" => __("coffeeMakerOperationStatus", __FILE__),
+    "supportedCommands" => __("supportedCommands", __FILE__),
+    "supportedCoffeeMakerOperationStatus" => __("supportedCoffeeMakerOperationStatus", __FILE__),
+    "supportedMenu" => __("supportedMenu", __FILE__),
+    "stop" => __("stop", __FILE__),
+    "makeCoffee" => __("makeCoffee", __FILE__),
+  ),
+  "consumableLife" => 
+  array (
+    "lifespan" => __("lifespan", __FILE__),
+    "type" => __("Remplacement de consommable", __FILE__),
+    "startDate" => __("Dernière date de remplacement", __FILE__),
+    "reset" => __("Réinitialiser", __FILE__),
+  ),
+  "containerState" => 
+  array (
+    "containerState" => __("État du conteneur", __FILE__),
+    "supportedContainerStates" => __("supported container states", __FILE__),
+    "content" => __("État du conteneur", __FILE__),
+  ),
+  "custom.supportedOptions" => 
+  array (
+    "course" => __("course", __FILE__),
+    "setCourse" => __("setCourse", __FILE__),
+  ),
+  "deliveryRobotCall" => 
+  array (
+    "robotState" => __("Robot de distribution", __FILE__),
+    "supportedRobotStates" => __("Supported delivery robot states", __FILE__),
+    "supportedRobotCommands" => __("Supported delivery robot commands", __FILE__),
+    "call" => __("Appeler", __FILE__),
+    "cancel" => __("Annulé", __FILE__),
+  ),
+  "dishwasherMode" => 
+  array (
+    "supportedDishwasherModes" => __("supported dishwasher modes", __FILE__),
+  ),
+  "dishwasherOperatingState" => 
+  array (
+    "progress" => __("progress", __FILE__),
+  ),
+  "endToEndEncryption" => 
+  array (
+    "nonce" => __("nonce", __FILE__),
+    "encryptedKek" => __("encryptedKek", __FILE__),
+    "generateNonce" => __("generateNonce", __FILE__),
+    "encryptKek" => __("encryptKek", __FILE__),
+  ),
+  "firmwareUpdate" => 
+  array (
+    "imageTransferProgress" => __("imageTransferProgress", __FILE__),
+    "estimatedTimeRemaining" => __("estimatedTimeRemaining", __FILE__),
+    "supportsProgressReports" => __("supportsProgressReports", __FILE__),
+  ),
+  "flowMeasurement" => 
+  array (
+    "flowRange" => __("flow range", __FILE__),
+    "flow" => __("Mesure du flux", __FILE__),
+  ),
+  "massageIntensityChange" => 
+  array (
+    "supportedPositions" => __("Supported Positions", __FILE__),
+    "nextIntensity" => __("Massage", __FILE__),
+  ),
+  "massageIntensityControl" => 
+  array (
+    "intensityFoot" => __("Intensité aux pieds", __FILE__),
+    "supportedPositions" => __("Supported Positions", __FILE__),
+    "intensityWhole" => __("Intensité", __FILE__),
+    "intensityHead" => __("Intensité à la tête", __FILE__),
+    "intensityRange" => __("Intensity of Range", __FILE__),
+    "setIntensityFoot" => __("setIntensityFoot", __FILE__),
+    "setIntensityWhole" => __("setIntensityWhole", __FILE__),
+    "setIntensityHead" => __("setIntensityHead", __FILE__),
+  ),
+  "massageOperating" => 
+  array (
+    "massageState" => __("Massage", __FILE__),
+    "stop" => __("Désactivé", __FILE__),
+    "start" => __("Activé", __FILE__),
+  ),
+  "massageOperatingState" => 
+  array (
+    "massageState" => __("Massage", __FILE__),
+  ),
+  "massageTimeChange" => 
+  array (
+    "nextTime" => __("Temps", __FILE__),
+  ),
+  "massageTimeControl" => 
+  array (
+    "completionTime" => __("Temps", __FILE__),
+    "supportedSetTimes" => __("Supported Set Times", __FILE__),
+    "setTime" => __("Temps", __FILE__),
+  ),
+  "motionBed" => 
+  array (
+    "mode" => __("Mouvement", __FILE__),
+    "supportedMotionPositions" => __("Supported Motion Positions", __FILE__),
+    "statelessMode" => __("Stateless Mode", __FILE__),
+    "supportedModes" => __("Supported Modes", __FILE__),
+    "stop" => __("stop", __FILE__),
+    "setMode" => __("Set mode", __FILE__),
+    "start" => __("start", __FILE__),
+  ),
+  "ovenSetpoint" => 
+  array (
+    "ovenSetpointRange" => __("Oven setpoint range", __FILE__),
+  ),
+  "petActivity" => 
+  array (
+    "supportedPetActivities" => __("Supported pet activities", __FILE__),
+    "petActivity" => __("Activité de l\"animal", __FILE__),
+  ),
+  "safetySwitch" => 
+  array (
+    "switch" => __("switch", __FILE__),
+    "off" => __("off", __FILE__),
+  ),
+  "safetyValve" => 
+  array (
+    "valve" => __("valve", __FILE__),
+    "close" => __("close", __FILE__),
+  ),
+  "samsungce.washerCycle" => 
+  array (
+    "cycleType" => __("cycleType", __FILE__),
+  ),
+  "samsungvd.ambient" => 
+  array (
+    "info" => __("info", __FILE__),
+  ),
+  "samsungvd.audioInputSource" => 
+  array (
+    "setNextInputSource" => __("setNextInputSource", __FILE__),
+  ),
+  "samsungvd.supportsFeatures" => 
+  array (
+    "remotelessSupported" => __("remotelessSupported", __FILE__),
+    "artSupported" => __("artSupported", __FILE__),
+  ),
+  "securitySystem" => 
+  array (
+    "supportedSecuritySystemCommands" => __("supported security system commands", __FILE__),
+    "supportedSecuritySystemStatuses" => __("supported security system statuses", __FILE__),
+  ),
+  "serviceArea" => 
+  array (
+    "selectedAreas" => __("Zone de nettoyage", __FILE__),
+    "supportedAreas" => __("supported areas", __FILE__),
+    "selectAreas" => __("select areas", __FILE__),
+  ),
+  "wifiInformation" => 
+  array (
+    "supportedWiFiAuthTypes" => __("supportedWiFiAuthTypes", __FILE__),
+    "ssid" => __("ssid", __FILE__),
+    "supportedWiFiFrequencies" => __("supportedWiFiFrequencies", __FILE__),
   ),
 );
