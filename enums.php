f32871af39e59cc63ab4f6329a10acecb6b4a0d8
Flobul <46993341+Flobul@users.noreply.github.com>
2025-05-01 09:06:11 +0000

    Auto check cmdname and enums of php file

--- enums.php
+++ enums.php
@@ -1,2 +1,112 @@
 <?php return array (
+  "state" => 
+  array (
+    "off" => __("off", __FILE__),
+    "preparing" => __("preparing", __FILE__),
+    "running" => __("running", __FILE__),
+    "stopped" => __("stopped", __FILE__),
+  ),
+  "chargingState" => 
+  array (
+    "other" => __("Other", __FILE__),
+  ),
+  "coffeeMakerOperationStatus" => 
+  array (
+    "initializing" => __("initializing", __FILE__),
+    "ready" => __("ready", __FILE__),
+    "making" => __("making", __FILE__),
+    "complete" => __("complete", __FILE__),
+    "error" => __("error", __FILE__),
+    "stopped" => __("stopped", __FILE__),
+  ),
+  "type" => 
+  array (
+    "desiccant" => __("Dessiccant", __FILE__),
+    "diffuser" => __("Diffuseur", __FILE__),
+    "filter" => __("Filtre", __FILE__),
+  ),
+  "containerState" => 
+  array (
+    "empty" => __("Vide", __FILE__),
+    "normal" => __("Normal", __FILE__),
+    "full" => __("Pleine", __FILE__),
+  ),
+  "content" => 
+  array (
+    "food" => __("Nourriture", __FILE__),
+    "water" => __("Eau", __FILE__),
+    "liquid" => __("Liquide", __FILE__),
+    "consumable" => __("Produit consommable", __FILE__),
+    "excretions" => __("Déjection animale", __FILE__),
+  ),
+  "acOptionalMode" => 
+  array (
+    "dryComfort" => __("Dry Comfort activé", __FILE__),
+  ),
+  "robotState" => 
+  array (
+    "available" => __("Mise en attente pour la livraison", __FILE__),
+    "using" => __("En cours d\"utilisation", __FILE__),
+    "error" => __("Erreur", __FILE__),
+    "called" => __("Appelé", __FILE__),
+    "callFailed" => __("Impossible de contacter le robot", __FILE__),
+  ),
+  "dishwasherMode" => 
+  array (
+    "smart" => __("Smart", __FILE__),
+    "daily" => __("Daily", __FILE__),
+    "superClean" => __("SuperClean", __FILE__),
+    "brightening" => __("Brightening", __FILE__),
+    "prewash" => __("Prewash", __FILE__),
+  ),
+  "mode" => 
+  array (
+    "preset1" => __("Ma trousse", __FILE__),
+    "preset2" => __("Preset 2", __FILE__),
+    "zeroG" => __("Gravité zéro", __FILE__),
+    "flat" => __("Plat", __FILE__),
+    "tv" => __("TV", __FILE__),
+    "reading" => __("Lecture", __FILE__),
+    "antiSnore" => __("Anti-ronflement", __FILE__),
+    "noMode" => __("noMode", __FILE__),
+  ),
+  "ovenMode" => 
+  array (
+    "StrongSteam" => __("Strong Steam", __FILE__),
+    "Rinse" => __("Rinse", __FILE__),
+  ),
+  "petActivity" => 
+  array (
+    "idle" => __("En veille", __FILE__),
+    "eating" => __("Alimentation", __FILE__),
+    "pooping" => __("Toilettes (déchets solides)", __FILE__),
+    "sleeping" => __("En veille", __FILE__),
+    "drinkingWater" => __("Hydratation", __FILE__),
+    "peeing" => __("Toilettes (urine)", __FILE__),
+    "playing" => __("Jeu", __FILE__),
+    "resting" => __("Repos", __FILE__),
+    "walking" => __("Marche", __FILE__),
+    "running" => __("Course", __FILE__),
+    "barking" => __("Aboiement", __FILE__),
+    "meowing" => __("Miaulement", __FILE__),
+  ),
+  "dryerJobState" => 
+  array (
+    "laundrySensing" => __("Détection du linge", __FILE__),
+  ),
+  "operatingState" => 
+  array (
+    "sabbath" => __("sabbath", __FILE__),
+    "suspend" => __("suspend", __FILE__),
+  ),
+  "cycleType" => 
+  array (
+    "allInOne" => __("allInOne", __FILE__),
+    "washingOnly" => __("washingOnly", __FILE__),
+    "dryingOnly" => __("dryingOnly", __FILE__),
+  ),
+  "protocolType" => 
+  array (
+    "l3_ocf" => __("l3_ocf", __FILE__),
+  ),
 );
