bd275b089c98e208dbe9a389aa504dd38fade7d9
Flobul <46993341+Flobul@users.noreply.github.com>
2025-09-13 10:48:56 +0000

    Auto check cmdname and enums of php file

--- cmdName.php
+++ cmdName.php
@@ -1,20 +1,81 @@
 <?php return array (
-  "alarmSystem" => 
+  "audioRecording" => 
   array (
-    "sendEvent(alarmSystemStatus,off)" => __("sendEvent(alarmSystemStatus,off)", __FILE__),
-    "sendEvent(alarmSystemStatus,away)" => __("sendEvent(alarmSystemStatus,away)", __FILE__),
-    "sendEvent(alarmSystemStatus,stay)" => __("sendEvent(alarmSystemStatus,stay)", __FILE__),
+    "audioRecording" => __("audioRecording", __FILE__),
+    "setAudioRecordingenabled" => "setAudioRecordingenabled",
+    "setAudioRecordingdisabled" => "setAudioRecordingdisabled",
   ),
-  "alarmSystemArmOnly" => 
+  "lockAliro" => 
   array (
-    "sendEvent(alarmSystemStatus,away)" => __("sendEvent(alarmSystemStatus,away)", __FILE__),
-    "sendEvent(alarmSystemStatus,stay)" => __("sendEvent(alarmSystemStatus,stay)", __FILE__),
+    "commandResult" => __("commandResult", __FILE__),
+    "groupResolvingKey" => __("groupResolvingKey", __FILE__),
+    "expeditedTransactionProtocolVersions" => __("expeditedTransactionProtocolVersions", __FILE__),
+    "credentials" => __("credentials", __FILE__),
+    "readerGroupIdentifier" => __("readerGroupIdentifier", __FILE__),
+    "bleAdvertisingVersion" => __("bleAdvertisingVersion", __FILE__),
+    "cardId" => __("cardId", __FILE__),
+    "bleUWBProtocolVersions" => __("bleUWBProtocolVersions", __FILE__),
+    "maxCredentialIssuerKeys" => __("maxCredentialIssuerKeys", __FILE__),
+    "maxEndpointKeys" => __("maxEndpointKeys", __FILE__),
+    "readerVerificationKey" => __("readerVerificationKey", __FILE__),
+    "setCredential" => __("setCredential", __FILE__),
+    "setCardId" => __("setCardId", __FILE__),
+    "setReaderConfig" => __("setReaderConfig", __FILE__),
+    "clearCredential" => __("clearCredential", __FILE__),
   ),
-  "astronomicalData" => 
+  "moldHealthConcern" => 
   array (
-    "sendEvent(sunriseTime)" => __("sendEvent(sunriseTime)", __FILE__),
-    "sendEvent(sunrise)" => __("sendEvent(sunrise)", __FILE__),
-    "sendEvent(sunset)" => __("sendEvent(sunset)", __FILE__),
-    "sendEvent(sunsetTime)" => __("sendEvent(sunsetTime)", __FILE__),
+    "supportedMoldValues" => __("supportedMoldValues", __FILE__),
+  ),
+  "plantCultivation" => 
+  array (
+    "supportedPlantType" => __("Supported plant types", __FILE__),
+    "supportedCommands" => __("Supported commands", __FILE__),
+    "startTime" => __("Jours de plantation", __FILE__),
+    "plantType" => __("Type de plante", __FILE__),
+    "resetStartTime" => __("Ajouter date plantation", __FILE__),
+    "setPlantType" => __("Set plant type", __FILE__),
+  ),
+  "samsungce.autoDispenseDetergent" => 
+  array (
+    "supportedTypes" => __("supportedTypes", __FILE__),
+  ),
+  "samsungce.robotCleanerPetCleaningSchedule" => 
+  array (
+    "excludeHolidays" => __("excludeHolidays", __FILE__),
+    "setExcludeHolidays" => __("setExcludeHolidays", __FILE__),
+  ),
+  "samsungce.robotCleanerPetMonitor" => 
+  array (
+    "excludeHolidays" => __("excludeHolidays", __FILE__),
+    "setExcludeHolidays" => __("setExcludeHolidays", __FILE__),
+  ),
+  "vehicleHvacRemoteSwitch" => 
+  array (
+    "off" => __("Désactiver", __FILE__),
+    "on" => __("Activer", __FILE__),
+  ),
+  "vehicleWarning" => 
+  array (
+    "lampWire" => __("Câble de lampe", __FILE__),
+    "auxiliaryBattery" => __("Batterie auxiliaire", __FILE__),
+    "brakeFluid" => __("Liquide de freins", __FILE__),
+    "fuel" => __("Niveau de carburant", __FILE__),
+    "tirePressureFrontLeft" => __("Avertissement sur la pression des pneus : avant gauche", __FILE__),
+    "tirePressureFrontRight" => __("Avertissement sur la pression des pneus : avant droit", __FILE__),
+    "washerFluid" => __("Liquide lave-glace", __FILE__),
+    "smartKeyBattery" => __("Batterie de la clé intelligente", __FILE__),
+    "engineOil" => __("Huile de moteur", __FILE__),
+    "supportedAttributes" => __("Supported attributes", __FILE__),
+    "tirePressureRearLeft" => __("Avertissement sur la pression des pneus : arrière gauche", __FILE__),
+    "tirePressureRearRight" => __("Avertissement sur la pression des pneus : arrière droit", __FILE__),
+  ),
+  "vehicleWindowState" => 
+  array (
+    "frontRightWindow" => __("Fenêtre avant droite", __FILE__),
+    "rearRightWindow" => __("Fenêtre arrière droite", __FILE__),
+    "supportedAttributes" => __("Supported attributes", __FILE__),
+    "frontLeftWindow" => __("Fenêtre avant gauche", __FILE__),
+    "rearLeftWindow" => __("Fenêtre arrière gauche", __FILE__),
   ),
 );
