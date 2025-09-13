31dbca10d3bdbcd521397b183044b7496dff513b
Flobul <46993341+Flobul@users.noreply.github.com>
2025-09-13 10:53:25 +0000

    Auto check cmdname and enums of php file

--- cmdName.php
+++ cmdName.php
@@ -1,20 +1,158 @@
 <?php return array (
-  "alarmSystem" => 
+  "audioRecording" => 
   array (
-    "sendEvent(alarmSystemStatus,off)" => __("sendEvent(alarmSystemStatus,off)", __FILE__),
-    "sendEvent(alarmSystemStatus,away)" => __("sendEvent(alarmSystemStatus,away)", __FILE__),
-    "sendEvent(alarmSystemStatus,stay)" => __("sendEvent(alarmSystemStatus,stay)", __FILE__),
+    "audioRecording" => __("audioRecording", __FILE__),
+    "setAudioRecordingenabled" => "setAudioRecordingenabled",
+    "setAudioRecordingdisabled" => "setAudioRecordingdisabled",
   ),
-  "alarmSystemArmOnly" => 
+  "imageControl" => 
   array (
-    "sendEvent(alarmSystemStatus,away)" => __("sendEvent(alarmSystemStatus,away)", __FILE__),
-    "sendEvent(alarmSystemStatus,stay)" => __("sendEvent(alarmSystemStatus,stay)", __FILE__),
+    "imageRotation" => __("imageRotation", __FILE__),
+    "imageFlipVertical" => __("imageFlipVertical", __FILE__),
+    "imageFlipHorizontal" => __("imageFlipHorizontal", __FILE__),
+    "supportedAttributes" => __("supportedAttributes", __FILE__),
+    "setImageFlipVerticalenabled" => "setImageFlipVerticalenabled",
+    "setImageFlipVerticaldisabled" => "setImageFlipVerticaldisabled",
+    "setImageRotation" => __("setImageRotation", __FILE__),
+    "setImageFlipHorizontalenabled" => "setImageFlipHorizontalenabled",
+    "setImageFlipHorizontaldisabled" => "setImageFlipHorizontaldisabled",
   ),
-  "astronomicalData" => 
+  "lockAliro" => 
   array (
-    "sendEvent(sunriseTime)" => __("sendEvent(sunriseTime)", __FILE__),
-    "sendEvent(sunrise)" => __("sendEvent(sunrise)", __FILE__),
-    "sendEvent(sunset)" => __("sendEvent(sunset)", __FILE__),
-    "sendEvent(sunsetTime)" => __("sendEvent(sunsetTime)", __FILE__),
+    "commandResult" => __("commandResult", __FILE__),
+    "groupResolvingKey" => __("groupResolvingKey", __FILE__),
+    "expeditedTransactionProtocolVersions" => __("expeditedTransactionProtocolVersions", __FILE__),
+    "credentials" => __("credentials", __FILE__),
+    "readerGroupIdentifier" => __("readerGroupIdentifier", __FILE__),
+    "bleAdvertisingVersion" => __("bleAdvertisingVersion", __FILE__),
+    "cardId" => __("cardId", __FILE__),
+    "bleUWBProtocolVersions" => __("bleUWBProtocolVersions", __FILE__),
+    "maxCredentialIssuerKeys" => __("maxCredentialIssuerKeys", __FILE__),
+    "maxEndpointKeys" => __("maxEndpointKeys", __FILE__),
+    "readerVerificationKey" => __("readerVerificationKey", __FILE__),
+    "setCredential" => __("setCredential", __FILE__),
+    "setCardId" => __("setCardId", __FILE__),
+    "setReaderConfig" => __("setReaderConfig", __FILE__),
+    "clearCredential" => __("clearCredential", __FILE__),
+  ),
+  "mechanicalPanTiltZoom" => 
+  array (
+    "maxPresets" => __("maxPresets", __FILE__),
+    "tiltRange" => __("tiltRange", __FILE__),
+    "presets" => __("presets", __FILE__),
+    "panRange" => __("panRange", __FILE__),
+    "zoom" => __("zoom", __FILE__),
+    "tilt" => __("tilt", __FILE__),
+    "pan" => __("pan", __FILE__),
+    "supportedAttributes" => __("supportedAttributes", __FILE__),
+    "zoomRange" => __("zoomRange", __FILE__),
+    "panRelative" => __("panRelative", __FILE__),
+    "tiltRelative" => __("tiltRelative", __FILE__),
+    "zoomRelative" => __("zoomRelative", __FILE__),
+    "setPanTiltZoom" => __("setPanTiltZoom", __FILE__),
+    "setZoom" => __("setZoom", __FILE__),
+    "setPan" => __("setPan", __FILE__),
+    "savePreset" => __("savePreset", __FILE__),
+    "moveToPreset" => __("moveToPreset", __FILE__),
+    "setTilt" => __("setTilt", __FILE__),
+  ),
+  "moldHealthConcern" => 
+  array (
+    "supportedMoldValues" => __("supportedMoldValues", __FILE__),
+  ),
+  "plantCultivation" => 
+  array (
+    "supportedPlantType" => __("Supported plant types", __FILE__),
+    "supportedCommands" => __("Supported commands", __FILE__),
+    "startTime" => __("Jours de plantation", __FILE__),
+    "plantType" => __("Type de plante", __FILE__),
+    "resetStartTime" => __("Ajouter date plantation", __FILE__),
+    "setPlantType" => __("Set plant type", __FILE__),
+  ),
+  "samsungce.autoDispenseDetergent" => 
+  array (
+    "supportedTypes" => __("supportedTypes", __FILE__),
+  ),
+  "samsungce.robotCleanerPetCleaningSchedule" => 
+  array (
+    "excludeHolidays" => __("excludeHolidays", __FILE__),
+    "setExcludeHolidays" => __("setExcludeHolidays", __FILE__),
+  ),
+  "samsungce.robotCleanerPetMonitor" => 
+  array (
+    "excludeHolidays" => __("excludeHolidays", __FILE__),
+    "setExcludeHolidays" => __("setExcludeHolidays", __FILE__),
+  ),
+  "threadBorderRouter" => 
+  array (
+    "threadVersion" => __("threadVersion", __FILE__),
+    "borderRouterName" => __("borderRouterName", __FILE__),
+    "threadInterfaceState" => __("threadInterfaceState", __FILE__),
+  ),
+  "threadNetwork" => 
+  array (
+    "panId" => __("panId", __FILE__),
+    "extendedPanId" => __("extendedPanId", __FILE__),
+    "networkName" => __("networkName", __FILE__),
+    "channel" => __("channel", __FILE__),
+    "networkKey" => __("networkKey", __FILE__),
+  ),
+  "vehicleBattery" => 
+  array (
+    "chargingRemainTime" => __("Temps de charge restant", __FILE__),
+    "chargingDetail" => __("Batterie", __FILE__),
+    "chargingPlug" => __("État du point de charge", __FILE__),
+    "batteryLevel" => __("Batterie", __FILE__),
+    "chargingState" => __("Statut de la charge", __FILE__),
+    "charge" => __("Démarrer la charge", __FILE__),
+    "stop" => __("Arrêter la charge", __FILE__),
+  ),
+  "vehicleDoorState" => 
+  array (
+    "frontLeftDoor" => __("Porte avant gauche", __FILE__),
+    "rearRightDoor" => __("Porte arrière droite", __FILE__),
+    "rearLeftDoor" => __("Porte arrière gauche", __FILE__),
+    "supportedAttributes" => __("Supported attributes", __FILE__),
+    "lockState" => __("Statut du verrouillage de la porte", __FILE__),
+    "frontRightDoor" => __("Porte avant droite", __FILE__),
+    "unlock" => __("Déverr.", __FILE__),
+    "lock" => __("Verr.", __FILE__),
+  ),
+  "vehicleHvac" => 
+  array (
+    "temperatureRange" => __("Temperature range", __FILE__),
+    "defogState" => __("Dégivrage avant", __FILE__),
+    "hvacSpeedRange" => __("Hvac fan speed range", __FILE__),
+    "temperature" => __("Température de l\"habitacle", __FILE__),
+    "hvacState" => __("Statut de la climatisation", __FILE__),
+    "hvacSpeed" => __("Vit. venti.", __FILE__),
+  ),
+  "vehicleHvacRemoteSwitch" => 
+  array (
+    "off" => __("Désactiver", __FILE__),
+    "on" => __("Activer", __FILE__),
+  ),
+  "vehicleWarning" => 
+  array (
+    "lampWire" => __("Câble de lampe", __FILE__),
+    "auxiliaryBattery" => __("Batterie auxiliaire", __FILE__),
+    "brakeFluid" => __("Liquide de freins", __FILE__),
+    "fuel" => __("Niveau de carburant", __FILE__),
+    "tirePressureFrontLeft" => __("Avertissement sur la pression des pneus : avant gauche", __FILE__),
+    "tirePressureFrontRight" => __("Avertissement sur la pression des pneus : avant droit", __FILE__),
+    "washerFluid" => __("Liquide lave-glace", __FILE__),
+    "smartKeyBattery" => __("Batterie de la clé intelligente", __FILE__),
+    "engineOil" => __("Huile de moteur", __FILE__),
+    "supportedAttributes" => __("Supported attributes", __FILE__),
+    "tirePressureRearLeft" => __("Avertissement sur la pression des pneus : arrière gauche", __FILE__),
+    "tirePressureRearRight" => __("Avertissement sur la pression des pneus : arrière droit", __FILE__),
+  ),
+  "vehicleWindowState" => 
+  array (
+    "frontRightWindow" => __("Fenêtre avant droite", __FILE__),
+    "rearRightWindow" => __("Fenêtre arrière droite", __FILE__),
+    "supportedAttributes" => __("Supported attributes", __FILE__),
+    "frontLeftWindow" => __("Fenêtre avant gauche", __FILE__),
+    "rearLeftWindow" => __("Fenêtre arrière gauche", __FILE__),
   ),
 );
