cc1e475352a21c6581e69039f3a2ac9fe152d939
Flobul <46993341+Flobul@users.noreply.github.com>
2026-03-25 17:47:20 +0000

    Auto check cmdname and enums of php file

--- enums.php
+++ enums.php
@@ -1,2 +1,55 @@
 <?php return array (
+  "doorState" => 
+  array (
+    "jammed" => __("jammed", __FILE__),
+    "forcedOpen" => __("forcedOpen", __FILE__),
+    "unspecifiedError" => __("unspecifiedError", __FILE__),
+    "ajar" => __("ajar", __FILE__),
+  ),
+  "mode" => 
+  array (
+    "auto" => __("auto", __FILE__),
+    "standard" => __("standard", __FILE__),
+    "power" => __("power", __FILE__),
+    "storage" => __("storage", __FILE__),
+    "clean" => __("clean", __FILE__),
+  ),
+  "state" => 
+  array (
+    "waiting" => __("waiting", __FILE__),
+    "drying" => __("drying", __FILE__),
+    "grinding" => __("grinding", __FILE__),
+    "cooling" => __("cooling", __FILE__),
+    "cleaning" => __("cleaning", __FILE__),
+    "completed" => __("completed", __FILE__),
+    "error" => __("error", __FILE__),
+  ),
+  "event" => 
+  array (
+    "error" => __("error", __FILE__),
+    "operationComplete" => __("operationComplete", __FILE__),
+    "cleaningComplete" => __("cleaningComplete", __FILE__),
+  ),
+  "alarm" => 
+  array (
+    "failedOpeningAttempt" => __("failedOpeningAttempt", __FILE__),
+  ),
+  "plantType" => 
+  array (
+    "parsley_italian" => __("parsley_italian", __FILE__),
+  ),
+  "robotCleanerMovement" => 
+  array (
+    "washingMop" => __("washingMop", __FILE__),
+  ),
+  "releaseCountry" => 
+  array (
+    "US" => __("US", __FILE__),
+    "KR" => __("KR", __FILE__),
+    "ZZ" => __("ZZ", __FILE__),
+  ),
+  "chargingDetail" => 
+  array (
+    "charging" => __("En charge", __FILE__),
+  ),
 );
