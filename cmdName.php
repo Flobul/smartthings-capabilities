7b07e62dea3a25fb3578df9a60b3e93f1af68acb
Flobul <46993341+Flobul@users.noreply.github.com>
2026-03-25 17:43:35 +0000

    Auto check cmdname and enums of php file

--- cmdName.php
+++ cmdName.php
@@ -1,20 +1,141 @@
 <?php return array (
-  "alarmSystem" => 
+  "cameraViewportSettings" => 
   array (
-    "sendEvent(alarmSystemStatus,off)" => __("sendEvent(alarmSystemStatus,off)", __FILE__),
-    "sendEvent(alarmSystemStatus,away)" => __("sendEvent(alarmSystemStatus,away)", __FILE__),
-    "sendEvent(alarmSystemStatus,stay)" => __("sendEvent(alarmSystemStatus,stay)", __FILE__),
+    "defaultViewport" => __("defaultViewport", __FILE__),
+    "minViewportResolution" => __("minViewportResolution", __FILE__),
+    "videoSensorParameters" => __("videoSensorParameters", __FILE__),
+    "setDefaultViewport" => __("setDefaultViewport", __FILE__),
   ),
-  "alarmSystemArmOnly" => 
+  "doorState" => 
   array (
-    "sendEvent(alarmSystemStatus,away)" => __("sendEvent(alarmSystemStatus,away)", __FILE__),
-    "sendEvent(alarmSystemStatus,stay)" => __("sendEvent(alarmSystemStatus,stay)", __FILE__),
+    "doorState" => __("doorState", __FILE__),
+    "supportedDoorStates" => __("supportedDoorStates", __FILE__),
   ),
-  "astronomicalData" => 
+  "endToEndEncryption" => 
   array (
-    "sendEvent(sunriseTime)" => __("sendEvent(sunriseTime)", __FILE__),
-    "sendEvent(sunrise)" => __("sendEvent(sunrise)", __FILE__),
-    "sendEvent(sunset)" => __("sendEvent(sunset)", __FILE__),
-    "sendEvent(sunsetTime)" => __("sendEvent(sunsetTime)", __FILE__),
+    "error" => __("error", __FILE__),
+  ),
+  "endToEndEncryptionState" => 
+  array (
+    "enabled" => __("enabled", __FILE__),
+    "setEnabledenabled" => "setEnabledenabled",
+    "setEnableddisabled" => "setEnableddisabled",
+  ),
+  "fanDirection" => 
+  array (
+    "fanDirection" => __("fanDirection", __FILE__),
+    "summerMode" => __("summerMode", __FILE__),
+    "winterMode" => __("winterMode", __FILE__),
+  ),
+  "foodWasteDryingGrinder" => 
+  array (
+    "mode" => __("mode", __FILE__),
+    "supportedStates" => __("supportedStates", __FILE__),
+    "state" => __("state", __FILE__),
+    "supportedModes" => __("supportedModes", __FILE__),
+    "event" => __("event", __FILE__),
+    "supportedEvents" => __("supportedEvents", __FILE__),
+    "stop" => __("stop", __FILE__),
+    "start" => __("start", __FILE__),
+    "setMode" => __("setMode", __FILE__),
+  ),
+  "knob" => 
+  array (
+    "rotateAmount" => __("rotateAmount", __FILE__),
+    "supportedAttributes" => __("supportedAttributes", __FILE__),
+    "heldRotateAmount" => __("heldRotateAmount", __FILE__),
+  ),
+  "localMediaStorage" => 
+  array (
+    "localSnapshotRecording" => __("localSnapshotRecording", __FILE__),
+    "localVideoRecording" => __("localVideoRecording", __FILE__),
+    "supportedAttributes" => __("supportedAttributes", __FILE__),
+    "setLocalVideoRecordingenabled" => "setLocalVideoRecordingenabled",
+    "setLocalVideoRecordingdisabled" => "setLocalVideoRecordingdisabled",
+    "setLocalSnapshotRecordingenabled" => "setLocalSnapshotRecordingenabled",
+    "setLocalSnapshotRecordingdisabled" => "setLocalSnapshotRecordingdisabled",
+  ),
+  "mechanicalPanTiltZoom" => 
+  array (
+    "removePreset" => __("removePreset", __FILE__),
+  ),
+  "robotCleanerOperatingState" => 
+  array (
+    "supportedCommands" => __("supportedCommands", __FILE__),
+  ),
+  "samsungce.autoDispenseDetergent" => 
+  array (
+    "supportedTypes" => __("supportedTypes", __FILE__),
+  ),
+  "samsungce.deviceIdentification" => 
+  array (
+    "releaseCountry" => __("releaseCountry", __FILE__),
+  ),
+  "samsungce.washerCycle" => 
+  array (
+    "availableCycleTypes" => __("availableCycleTypes", __FILE__),
+    "setCycleType" => __("setCycleType", __FILE__),
+  ),
+  "samsungce.washerOperatingState" => 
+  array (
+    "washingProgress" => __("washingProgress", __FILE__),
+    "dryingProgress" => __("dryingProgress", __FILE__),
+  ),
+  "samsungvd.ambient" => 
+  array (
+    "sendData" => __("sendData", __FILE__),
+  ),
+  "soilMoistureMeasurement" => 
+  array (
+    "soilMoistureLevel" => __("soilMoistureLevel", __FILE__),
+    "soilMoistureLevelRange" => __("soilMoistureLevelRange", __FILE__),
+  ),
+  "sounds" => 
+  array (
+    "selectedSound" => __("selectedSound", __FILE__),
+    "supportedSounds" => __("supportedSounds", __FILE__),
+    "playSound" => __("playSound", __FILE__),
+    "setSelectedSound" => __("setSelectedSound", __FILE__),
+  ),
+  "statelessColorTemperatureStep" => 
+  array (
+    "stepColorTemperatureByPercent" => __("stepColorTemperatureByPercent", __FILE__),
+  ),
+  "statelessSwitchLevelStep" => 
+  array (
+    "stepLevel" => __("stepLevel", __FILE__),
+  ),
+  "vehicleWarning" => 
+  array (
+    "electricVehicleBattery" => __("Batterie faible", __FILE__),
+  ),
+  "videoStreamSettings" => 
+  array (
+    "supportedFeatures" => __("supportedFeatures", __FILE__),
+    "videoStreams" => __("videoStreams", __FILE__),
+    "supportedResolutions" => __("supportedResolutions", __FILE__),
+    "setStream" => __("setStream", __FILE__),
+  ),
+  "webrtc" => 
+  array (
+    "talkbackDuplex" => __("talkbackDuplex", __FILE__),
+    "standbyMode" => __("standbyMode", __FILE__),
+    "solicitOffer" => __("solicitOffer", __FILE__),
+  ),
+  "zoneManagement" => 
+  array (
+    "supportedFeatures" => __("supportedFeatures", __FILE__),
+    "maxZones" => __("maxZones", __FILE__),
+    "sensitivity" => __("sensitivity", __FILE__),
+    "zones" => __("zones", __FILE__),
+    "triggers" => __("triggers", __FILE__),
+    "sensitivityRange" => __("sensitivityRange", __FILE__),
+    "triggeredZones" => __("triggeredZones", __FILE__),
+    "removeTrigger" => __("removeTrigger", __FILE__),
+    "newZone" => __("newZone", __FILE__),
+    "setSensitivity" => __("setSensitivity", __FILE__),
+    "updateZone" => __("updateZone", __FILE__),
+    "removeZone" => __("removeZone", __FILE__),
+    "createOrUpdateTrigger" => __("createOrUpdateTrigger", __FILE__),
   ),
 );
