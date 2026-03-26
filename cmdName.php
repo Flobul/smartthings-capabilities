a8e4ee469d5e5394ab975735602b3db909d0c118
Flobul <46993341+Flobul@users.noreply.github.com>
2026-03-26 06:49:15 +0000

    Auto check cmdname and enums of php file

--- cmdName.php
+++ cmdName.php
@@ -1,20 +1,340 @@
 <?php return array (
-  "alarmSystem" => 
+  "audioRecording" => 
   array (
-    "sendEvent(alarmSystemStatus,off)" => __("sendEvent(alarmSystemStatus,off)", __FILE__),
-    "sendEvent(alarmSystemStatus,away)" => __("sendEvent(alarmSystemStatus,away)", __FILE__),
-    "sendEvent(alarmSystemStatus,stay)" => __("sendEvent(alarmSystemStatus,stay)", __FILE__),
+    "audioRecording" => __("audioRecording", __FILE__),
+    "setAudioRecordingenabled" => "setAudioRecordingenabled",
+    "setAudioRecordingdisabled" => "setAudioRecordingdisabled",
   ),
-  "alarmSystemArmOnly" => 
+  "cameraPrivacyMode" => 
   array (
-    "sendEvent(alarmSystemStatus,away)" => __("sendEvent(alarmSystemStatus,away)", __FILE__),
-    "sendEvent(alarmSystemStatus,stay)" => __("sendEvent(alarmSystemStatus,stay)", __FILE__),
+    "softLivestreamPrivacyMode" => __("softLivestreamPrivacyMode", __FILE__),
+    "softRecordingPrivacyMode" => __("softRecordingPrivacyMode", __FILE__),
+    "supportedCommands" => __("supportedCommands", __FILE__),
+    "hardPrivacyMode" => __("hardPrivacyMode", __FILE__),
+    "supportedAttributes" => __("supportedAttributes", __FILE__),
+    "setSoftLivestreamPrivacyModeenabled" => "setSoftLivestreamPrivacyModeenabled",
+    "setSoftLivestreamPrivacyModedisabled" => "setSoftLivestreamPrivacyModedisabled",
+    "setSoftRecordingPrivacyModeenabled" => "setSoftRecordingPrivacyModeenabled",
+    "setSoftRecordingPrivacyModedisabled" => "setSoftRecordingPrivacyModedisabled",
   ),
-  "astronomicalData" => 
+  "cameraViewportSettings" => 
   array (
-    "sendEvent(sunriseTime)" => __("sendEvent(sunriseTime)", __FILE__),
-    "sendEvent(sunrise)" => __("sendEvent(sunrise)", __FILE__),
-    "sendEvent(sunset)" => __("sendEvent(sunset)", __FILE__),
-    "sendEvent(sunsetTime)" => __("sendEvent(sunsetTime)", __FILE__),
+    "defaultViewport" => __("defaultViewport", __FILE__),
+    "minViewportResolution" => __("minViewportResolution", __FILE__),
+    "videoSensorParameters" => __("videoSensorParameters", __FILE__),
+    "setDefaultViewport" => __("setDefaultViewport", __FILE__),
+  ),
+  "coffeeMakerOperation" => 
+  array (
+    "temperatureRange" => __("Temperature range", __FILE__),
+    "supportedArgument" => __("Arguments", __FILE__),
+    "waterAmountRange" => __("Water amount range", __FILE__),
+    "coffeeAmountRange" => __("Coffee amount range", __FILE__),
+    "coffeeStrengthRange" => __("Coffee strength range", __FILE__),
+    "milkAmountRange" => __("Milk amount range", __FILE__),
+    "makeCustomizedCoffee" => __("Make manual coffee", __FILE__),
+  ),
+  "custom.fridgeMode" => 
+  array (
+    "supportedFullFridgeModes" => __("supportedFullFridgeModes", __FILE__),
+  ),
+  "doorState" => 
+  array (
+    "doorState" => __("doorState", __FILE__),
+    "supportedDoorStates" => __("supportedDoorStates", __FILE__),
+  ),
+  "endToEndEncryption" => 
+  array (
+    "error" => __("error", __FILE__),
+  ),
+  "endToEndEncryptionState" => 
+  array (
+    "enabled" => __("enabled", __FILE__),
+    "setEnabledenabled" => "setEnabledenabled",
+    "setEnableddisabled" => "setEnableddisabled",
+  ),
+  "fanDirection" => 
+  array (
+    "fanDirection" => __("fanDirection", __FILE__),
+    "summerMode" => __("summerMode", __FILE__),
+    "winterMode" => __("winterMode", __FILE__),
+  ),
+  "foodWasteDryingGrinder" => 
+  array (
+    "mode" => __("mode", __FILE__),
+    "supportedStates" => __("supportedStates", __FILE__),
+    "state" => __("state", __FILE__),
+    "supportedModes" => __("supportedModes", __FILE__),
+    "event" => __("event", __FILE__),
+    "supportedEvents" => __("supportedEvents", __FILE__),
+    "stop" => __("stop", __FILE__),
+    "start" => __("start", __FILE__),
+    "setMode" => __("setMode", __FILE__),
+  ),
+  "hdr" => 
+  array (
+    "hdr" => __("hdr", __FILE__),
+    "setHdrenabled" => "setHdrenabled",
+    "setHdrdisabled" => "setHdrdisabled",
+  ),
+  "imageControl" => 
+  array (
+    "imageRotation" => __("imageRotation", __FILE__),
+    "imageFlipVertical" => __("imageFlipVertical", __FILE__),
+    "imageFlipHorizontal" => __("imageFlipHorizontal", __FILE__),
+    "supportedAttributes" => __("supportedAttributes", __FILE__),
+    "setImageFlipVerticalenabled" => "setImageFlipVerticalenabled",
+    "setImageFlipVerticaldisabled" => "setImageFlipVerticaldisabled",
+    "setImageRotation" => __("setImageRotation", __FILE__),
+    "setImageFlipHorizontalenabled" => "setImageFlipHorizontalenabled",
+    "setImageFlipHorizontaldisabled" => "setImageFlipHorizontaldisabled",
+  ),
+  "knob" => 
+  array (
+    "rotateAmount" => __("rotateAmount", __FILE__),
+    "supportedAttributes" => __("supportedAttributes", __FILE__),
+    "heldRotateAmount" => __("heldRotateAmount", __FILE__),
+  ),
+  "localMediaStorage" => 
+  array (
+    "localSnapshotRecording" => __("localSnapshotRecording", __FILE__),
+    "localVideoRecording" => __("localVideoRecording", __FILE__),
+    "supportedAttributes" => __("supportedAttributes", __FILE__),
+    "setLocalVideoRecordingenabled" => "setLocalVideoRecordingenabled",
+    "setLocalVideoRecordingdisabled" => "setLocalVideoRecordingdisabled",
+    "setLocalSnapshotRecordingenabled" => "setLocalSnapshotRecordingenabled",
+    "setLocalSnapshotRecordingdisabled" => "setLocalSnapshotRecordingdisabled",
+  ),
+  "lockAliro" => 
+  array (
+    "commandResult" => __("commandResult", __FILE__),
+    "groupResolvingKey" => __("groupResolvingKey", __FILE__),
+    "expeditedTransactionProtocolVersions" => __("expeditedTransactionProtocolVersions", __FILE__),
+    "credentials" => __("credentials", __FILE__),
+    "readerGroupIdentifier" => __("readerGroupIdentifier", __FILE__),
+    "bleAdvertisingVersion" => __("bleAdvertisingVersion", __FILE__),
+    "cardId" => __("cardId", __FILE__),
+    "bleUWBProtocolVersions" => __("bleUWBProtocolVersions", __FILE__),
+    "maxCredentialIssuerKeys" => __("maxCredentialIssuerKeys", __FILE__),
+    "maxEndpointKeys" => __("maxEndpointKeys", __FILE__),
+    "readerVerificationKey" => __("readerVerificationKey", __FILE__),
+    "setEndpointKey" => __("setEndpointKey", __FILE__),
+    "setCardId" => __("setCardId", __FILE__),
+    "setReaderConfig" => __("setReaderConfig", __FILE__),
+    "setIssuerKey" => __("setIssuerKey", __FILE__),
+    "clearIssuerKey" => __("clearIssuerKey", __FILE__),
+    "clearEndpointKey" => __("clearEndpointKey", __FILE__),
+  ),
+  "lockCodes" => 
+  array (
+    "migrated" => __("migrated", __FILE__),
+    "migrate" => __("migrate", __FILE__),
+  ),
+  "mechanicalPanTiltZoom" => 
+  array (
+    "maxPresets" => __("maxPresets", __FILE__),
+    "tiltRange" => __("tiltRange", __FILE__),
+    "presets" => __("presets", __FILE__),
+    "panRange" => __("panRange", __FILE__),
+    "zoom" => __("zoom", __FILE__),
+    "tilt" => __("tilt", __FILE__),
+    "pan" => __("pan", __FILE__),
+    "supportedAttributes" => __("supportedAttributes", __FILE__),
+    "zoomRange" => __("zoomRange", __FILE__),
+    "panRelative" => __("panRelative", __FILE__),
+    "tiltRelative" => __("tiltRelative", __FILE__),
+    "removePreset" => __("removePreset", __FILE__),
+    "zoomRelative" => __("zoomRelative", __FILE__),
+    "setPanTiltZoom" => __("setPanTiltZoom", __FILE__),
+    "setZoom" => __("setZoom", __FILE__),
+    "setPan" => __("setPan", __FILE__),
+    "savePreset" => __("savePreset", __FILE__),
+    "moveToPreset" => __("moveToPreset", __FILE__),
+    "setTilt" => __("setTilt", __FILE__),
+  ),
+  "moldHealthConcern" => 
+  array (
+    "supportedMoldValues" => __("Supported mold values", __FILE__),
+  ),
+  "nightVision" => 
+  array (
+    "illumination" => __("illumination", __FILE__),
+    "nightVision" => __("nightVision", __FILE__),
+    "supportedAttributes" => __("supportedAttributes", __FILE__),
+    "setIllumination" => __("setIllumination", __FILE__),
+    "setNightVision" => __("setNightVision", __FILE__),
+  ),
+  "plantCultivation" => 
+  array (
+    "supportedPlantType" => __("Supported plant types", __FILE__),
+    "supportedCommands" => __("Supported commands", __FILE__),
+    "startTime" => __("Jours de plantation", __FILE__),
+    "plantType" => __("Type de plante", __FILE__),
+    "resetStartTime" => __("Ajouter date plantation", __FILE__),
+    "setPlantType" => __("Set plant type", __FILE__),
+  ),
+  "robotCleanerOperatingState" => 
+  array (
+    "supportedCommands" => __("supported commands", __FILE__),
+  ),
+  "samsungce.autoDispenseDetergent" => 
+  array (
+    "supportedTypes" => __("supportedTypes", __FILE__),
+  ),
+  "samsungce.deviceIdentification" => 
+  array (
+    "releaseCountry" => __("releaseCountry", __FILE__),
+  ),
+  "samsungce.robotCleanerPetCleaningSchedule" => 
+  array (
+    "excludeHolidays" => __("excludeHolidays", __FILE__),
+    "setExcludeHolidays" => __("setExcludeHolidays", __FILE__),
+  ),
+  "samsungce.robotCleanerPetMonitor" => 
+  array (
+    "excludeHolidays" => __("excludeHolidays", __FILE__),
+    "setExcludeHolidays" => __("setExcludeHolidays", __FILE__),
+  ),
+  "samsungce.washerCycle" => 
+  array (
+    "availableCycleTypes" => __("availableCycleTypes", __FILE__),
+    "setCycleType" => __("setCycleType", __FILE__),
+  ),
+  "samsungce.washerOperatingState" => 
+  array (
+    "washingProgress" => __("washingProgress", __FILE__),
+    "dryingProgress" => __("dryingProgress", __FILE__),
+  ),
+  "samsungvd.ambient" => 
+  array (
+    "sendData" => __("sendData", __FILE__),
+  ),
+  "soilMoistureMeasurement" => 
+  array (
+    "soilMoistureLevel" => __("soilMoistureLevel", __FILE__),
+    "soilMoistureLevelRange" => __("soilMoistureLevelRange", __FILE__),
+  ),
+  "sounds" => 
+  array (
+    "selectedSound" => __("selectedSound", __FILE__),
+    "supportedSounds" => __("supportedSounds", __FILE__),
+    "playSound" => __("playSound", __FILE__),
+    "setSelectedSound" => __("setSelectedSound", __FILE__),
+  ),
+  "statelessColorTemperatureStep" => 
+  array (
+    "stepColorTemperatureByPercent" => __("stepColorTemperatureByPercent", __FILE__),
+  ),
+  "statelessSwitchLevelStep" => 
+  array (
+    "stepLevel" => __("stepLevel", __FILE__),
+  ),
+  "threadBorderRouter" => 
+  array (
+    "threadVersion" => __("threadVersion", __FILE__),
+    "borderRouterName" => __("borderRouterName", __FILE__),
+    "threadInterfaceState" => __("threadInterfaceState", __FILE__),
+  ),
+  "threadNetwork" => 
+  array (
+    "panId" => __("panId", __FILE__),
+    "extendedPanId" => __("extendedPanId", __FILE__),
+    "networkName" => __("networkName", __FILE__),
+    "channel" => __("channel", __FILE__),
+    "networkKey" => __("networkKey", __FILE__),
+  ),
+  "vehicleBattery" => 
+  array (
+    "chargingRemainTime" => __("Temps restant", __FILE__),
+    "chargingDetail" => __("Batterie", __FILE__),
+    "chargingPlug" => __("État du point de charge", __FILE__),
+    "batteryLevel" => __("Batterie", __FILE__),
+    "chargingState" => __("Statut de la charge", __FILE__),
+    "charge" => __("Démarrer la charge", __FILE__),
+    "stop" => __("Arrêter la charge", __FILE__),
+  ),
+  "vehicleDoorState" => 
+  array (
+    "frontLeftDoor" => __("Avant gauche", __FILE__),
+    "rearRightDoor" => __("Arrière droit", __FILE__),
+    "rearLeftDoor" => __("Arrière gauche", __FILE__),
+    "supportedAttributes" => __("Supported attributes", __FILE__),
+    "lockState" => __("État de verrouillage", __FILE__),
+    "frontRightDoor" => __("Avant droit", __FILE__),
+    "unlock" => __("Déverr.", __FILE__),
+    "lock" => __("Verr.", __FILE__),
+  ),
+  "vehicleHvac" => 
+  array (
+    "temperatureRange" => __("Temperature range", __FILE__),
+    "defogState" => __("Dégivrage avant", __FILE__),
+    "hvacSpeedRange" => __("Hvac fan speed range", __FILE__),
+    "temperature" => __("Température de l\"habitacle", __FILE__),
+    "hvacState" => __("Statut", __FILE__),
+    "hvacSpeed" => __("Vit. venti.", __FILE__),
+  ),
+  "vehicleHvacRemoteSwitch" => 
+  array (
+    "off" => __("Éteindre le contrôle de la climatisation/moteur", __FILE__),
+    "on" => __("Allumer le contrôle de la climatisation/moteur", __FILE__),
+  ),
+  "vehicleWarning" => 
+  array (
+    "fuel" => __("Niveau de carburant", __FILE__),
+    "tirePressureFrontLeft" => __("Pression des pneus : avant gauche", __FILE__),
+    "electricVehicleBattery" => __("Batterie du véhicule", __FILE__),
+    "supportedAttributes" => __("Supported attributes", __FILE__),
+    "lampWire" => __("Câble de lampe", __FILE__),
+    "auxiliaryBattery" => __("Batterie auxiliaire", __FILE__),
+    "brakeFluid" => __("Liquide de freins", __FILE__),
+    "tirePressureFrontRight" => __("Pression des pneus : avant droit", __FILE__),
+    "washerFluid" => __("Liquide lave-glace", __FILE__),
+    "smartKeyBattery" => __("Batterie de la clé intelligente", __FILE__),
+    "engineOil" => __("Huile de moteur", __FILE__),
+    "tirePressureRearLeft" => __("Pression des pneus : arrière gauche", __FILE__),
+    "tirePressureRearRight" => __("Pression des pneus : arrière droit", __FILE__),
+  ),
+  "vehicleWindowState" => 
+  array (
+    "frontRightWindow" => __("Avant droit", __FILE__),
+    "rearRightWindow" => __("Arrière droit", __FILE__),
+    "supportedAttributes" => __("Supported attributes", __FILE__),
+    "frontLeftWindow" => __("Avant gauche", __FILE__),
+    "rearLeftWindow" => __("Arrière gauche", __FILE__),
+  ),
+  "videoStreamSettings" => 
+  array (
+    "supportedFeatures" => __("supportedFeatures", __FILE__),
+    "videoStreams" => __("videoStreams", __FILE__),
+    "supportedResolutions" => __("supportedResolutions", __FILE__),
+    "setStream" => __("setStream", __FILE__),
+  ),
+  "webrtc" => 
+  array (
+    "talkbackDuplex" => __("Talkback duplex", __FILE__),
+    "standbyMode" => __("Standby mode", __FILE__),
+    "solicitOffer" => __("solicitOffer", __FILE__),
+  ),
+  "windowShadePreset" => 
+  array (
+    "supportedCommands" => __("Supported commands", __FILE__),
+    "position" => __("Position préréglée", __FILE__),
+    "setPresetPosition" => __("Set preset position", __FILE__),
+  ),
+  "zoneManagement" => 
+  array (
+    "supportedFeatures" => __("supportedFeatures", __FILE__),
+    "maxZones" => __("maxZones", __FILE__),
+    "sensitivity" => __("sensitivity", __FILE__),
+    "zones" => __("zones", __FILE__),
+    "triggers" => __("triggers", __FILE__),
+    "sensitivityRange" => __("sensitivityRange", __FILE__),
+    "triggeredZones" => __("triggeredZones", __FILE__),
+    "removeTrigger" => __("removeTrigger", __FILE__),
+    "newZone" => __("newZone", __FILE__),
+    "setSensitivity" => __("setSensitivity", __FILE__),
+    "updateZone" => __("updateZone", __FILE__),
+    "removeZone" => __("removeZone", __FILE__),
+    "createOrUpdateTrigger" => __("createOrUpdateTrigger", __FILE__),
   ),
 );
