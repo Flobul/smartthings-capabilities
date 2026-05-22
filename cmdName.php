a1e3021243678bbd887adf59fc9225c18ea62a04
Flobul <46993341+Flobul@users.noreply.github.com>
2026-05-22 14:06:49 +0000

    Auto check cmdname and enums of php file

--- cmdName.php
+++ cmdName.php
@@ -1,4 +1,10 @@
 <?php return array (
+  "nightVision" => 
+  array (
+    "nightVisionMode" => __("nightVisionMode", __FILE__),
+    "setNightVisionModeColor" => "setNightVisionModeColor",
+    "setNightVisionModeBlackAndWhite" => "setNightVisionModeBlackAndWhite",
+  ),
   "samsungce.autoDispenseDetergent" => 
   array (
     "supportedTypes" => __("supportedTypes", __FILE__),
