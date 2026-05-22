1f25efea9c1fee1e2e62ca771522cdb775df0385
Flobul <46993341+Flobul@users.noreply.github.com>
2026-05-22 14:07:38 +0000

    Auto check cmdname and enums of php file

--- cmdName.php
+++ cmdName.php
@@ -1,4 +1,22 @@
 <?php return array (
+  "clipDuration" => 
+  array (
+    "clipDuration" => __("clipDuration", __FILE__),
+    "supportedMaxDuration" => __("supportedMaxDuration", __FILE__),
+    "setClipDuration" => __("setClipDuration", __FILE__),
+  ),
+  "motionTracking" => 
+  array (
+    "enabledTargets" => __("enabledTargets", __FILE__),
+    "supportedTargets" => __("supportedTargets", __FILE__),
+    "setTrackingTargets" => __("setTrackingTargets", __FILE__),
+  ),
+  "nightVision" => 
+  array (
+    "nightVisionMode" => __("nightVisionMode", __FILE__),
+    "setNightVisionModeColor" => "setNightVisionModeColor",
+    "setNightVisionModeBlackAndWhite" => "setNightVisionModeBlackAndWhite",
+  ),
   "samsungce.autoDispenseDetergent" => 
   array (
     "supportedTypes" => __("supportedTypes", __FILE__),
